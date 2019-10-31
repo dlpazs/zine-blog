ce3cde95ddadce24230c98c37990c6201c6cfd98
Parulian Saragi <paruliansaragi>
2019-10-31 19:48:35 +0000

    Updates

---
+++ precache-manifest.b756b4145369b5d2a27305aa2399296b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ea5c90bedf7d54490c038dd7328e08",
+    "url": "/zine-blog/index.html"
+  },
+  {
+    "revision": "2bbda71f6f4fc0261960",
+    "url": "/zine-blog/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f77d3b42f58650f7836a",
+    "url": "/zine-blog/static/js/2.9aa06fe4.chunk.js"
+  },
+  {
+    "revision": "2bbda71f6f4fc0261960",
+    "url": "/zine-blog/static/js/main.eb531626.chunk.js"
+  },
+  {
+    "revision": "9ab2462c1aefb8a7e62e",
+    "url": "/zine-blog/static/js/runtime-main.157f8bc4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/zine-blog/static/media/logo.25bf045c.svg"
+  }
+]);
