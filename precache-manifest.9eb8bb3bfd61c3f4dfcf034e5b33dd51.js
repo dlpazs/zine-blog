874a8d9f8e870b797205d6bd08303b9ee0a5e18e
Parulian Saragi <paruliansaragi>
2019-10-31 20:32:54 +0000

    Updates

--- precache-manifest.9eb8bb3bfd61c3f4dfcf034e5b33dd51.js
+++ precache-manifest.9eb8bb3bfd61c3f4dfcf034e5b33dd51.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "be200f08d695a4ccba43639b26f2faee",
+    "revision": "51aa8bc466200b431d3aa5d0823f4703",
     "url": "/zine-blog/index.html"
   },
   {
-    "revision": "5020b7a0e221f59cd759",
+    "revision": "3e89ec1d820eab00dc87",
     "url": "/zine-blog/static/css/main.b100e6da.chunk.css"
   },
   {
-    "revision": "0960df7a263bf1595579",
-    "url": "/zine-blog/static/js/2.2c0ad7a8.chunk.js"
+    "revision": "d1897f3000c95bc6d062",
+    "url": "/zine-blog/static/js/2.9d4ff8e9.chunk.js"
   },
   {
-    "revision": "5020b7a0e221f59cd759",
-    "url": "/zine-blog/static/js/main.33190ddc.chunk.js"
+    "revision": "3e89ec1d820eab00dc87",
+    "url": "/zine-blog/static/js/main.2f326184.chunk.js"
   },
   {
     "revision": "9ab2462c1aefb8a7e62e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zine-blog/precache-manifest.a72d4bdbd53ddd2080a8f69bbc592db3.js"
+  "/zine-blog/precache-manifest.9eb8bb3bfd61c3f4dfcf034e5b33dd51.js"
 );
 
 self.addEventListener('message', (event) => {
